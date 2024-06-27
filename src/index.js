62809087669f50ef5494343a05705c8ac4127748
sayyedshayan <shayansayyed103@gmail.com>
2024-06-27 21:10:33 +0530

    connect to mongo atles

---
+++ src/index.js
@@ -0,0 +1,55 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+});
+
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ( async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error" ,(error) =>{
+//             console.log("error ", error);
+//             throw error
+//         })
+
+//         app.listen(process.env.PORT, ()=>{
+//             console.log(`app is listing on port ${process.env.PORT}`)
+//         })
+
+//     } catch (error) {
+//         console.error("ERROR: ", error)
+//         throw err
+//     }
+// })()
